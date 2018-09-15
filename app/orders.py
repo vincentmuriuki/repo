6d5da6f3473d1386d2544cda0caf44d69bde685c
Vincent <vincentmuriuki42@gmail.com>
2018-09-15 12:41:16 +0300

    [API endpoint to fetch specific order #160286108]

--- app/orders.py
+++ app/orders.py
@@ -3,32 +3,32 @@ from flask_restful import    Resource
 from .models import Order, orders
 
 
-# class SpecificOrder(Resource):
+class SpecificOrder(Resource):
     
 
-#     def get(self, id):
-#         order = Order().get_by_id(id)
+    def get(self, id):
+        order = Order().get_by_id(id)
         
-#         if order:
-#             return {"order": order.serialize()}, 200
+        if order:
+            return {"order": order.serialize()}, 200
 
-#         return {"message":"Order not found"}, 404
+        return {"message":"Order not found"}, 404
 
-#     def delete(self, id):
-#         order = Order().get_by_id(id)
+    # def delete(self, id):
+    #     order = Order().get_by_id(id)
 
-#         if order:
-#             orders.remove(order)
-#             return {"message":"order deleted successfully"},200
-#         return {"message":"Order not found"}, 404
+    #     if order:
+    #         orders.remove(order)
+    #         return {"message":"order deleted successfully"},200
+    #     return {"message":"Order not found"}, 404
 
-#     def put(self, id):
-#         order = Order().get_by_id(id)
+    # def put(self, id):
+    #     order = Order().get_by_id(id)
 
-#         if order:
-#             order.status="approved"
-#             return {"message":"status approved"}
-#         return {"message":"Order not found"}, 404
+    #     if order:
+    #         order.status="approved"
+    #         return {"message":"status approved"}
+    #     return {"message":"Order not found"}, 404
 
 class NewOrders(Resource):
 

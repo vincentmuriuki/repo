d5bd69b9977e69db8f2a7b783714a03c19c4921c
Vincent <vincentmuriuki42@gmail.com>
2018-09-15 13:09:14 +0300

    config file

---
+++ config.py
@@ -0,0 +1,23 @@
+class Config:
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+    TESTING=False
+
+
+class TestingConfig(Config):
+    DEBUG=True
+    TESTING=True
+    
+
+class ProductionConfig(Config):
+    pass
+
+
+app_config={
+    "development":DevelopmentConfig,
+    "testing":TestingConfig,
+    "production":ProductionConfig,
+    "default":DevelopmentConfig
+}
